d43afcd29800d5cc0e1f9a6b515ce753fd289ef4
Ruben Wagner <wagnerruben551@gmail.com>
2017-07-19 12:23:33 -0500

    project manager

--- scripts.js
+++ scripts.js
@@ -1,8 +1,34 @@
-function submitContactForm(){
-    var x = document.forms["contactForm"]["email"].value;
-    if(x ==""){
+var projects = [
+    {
+        "name": "Claim Academy"
+        "image":
+    } 
+];
+
+$(document).ready(function() {
+    
+    $('#contactForm').on('submit', submitContactForm);
+
+});
+
+function submitContactForm() {
+    event.preventDefault();
+
+    var x = ["email"].value;
+
+    if (x == "") {
         alert("Email must be filled out");
+        return false;
     } else {
-        alert('Thank you for submitting the form');
+        $(this).slideUp();
+        $("#success").slideDown();
+        //alert('Thank you for submitting the form');
     }
 }
+
+$(document).ready(function() {
+
+$.each(projects), function(index, project) {
+    console.log(project);
+    $('#projects').append('<a href="' + project.link +'"target=_blank"><img src="project.image"' + + '"/>' + project.name + '</a>');
+});
